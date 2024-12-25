e8c77a07279bee7c8ae16439c1b963bfc8c434c8
RaimaAftab95 <raima.aftab2022@gmail.com>
2024-12-25 09:47:18 +0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import './App.css'
 
-
-
 function App() {
-
 
 
   return (
     <>
       <h1 className="text-3xl font-bold underline">
-
-
         zenkly here!
-        
-        
-    </h1>
+      </h1>
+      <p>prettier and husky not working as expected</p>
     </>
   )
 }
