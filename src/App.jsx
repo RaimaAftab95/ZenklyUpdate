0ab8764fc913591ccff69cef4dc87f4f22dba640
RaimaAftab95 <raima.aftab2022@gmail.com>
2024-12-30 11:54:31 +0500

    Add generate idea page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Signup from './pages/Signup';
 import CreateProject from './pages/CreateProject';
+import GenerateIdea from './pages/GenerateIdea';
 import 'font-awesome/css/font-awesome.min.css';
 
 export default function App() {
@@ -9,7 +10,9 @@ export default function App() {
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/create-project" element={<CreateProject />} />
+        <Route path="/generate-idea" element={<GenerateIdea />} />
         <Route path="*" element={<Navigate to="/signup" />} />
+        <Route path="/generate-project" element={<CreateProject />} />
       </Routes>
     </div>
   );
