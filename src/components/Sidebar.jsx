aa416d68698cc66f670df347670b24140ee266e3
RaimaAftab95 <raima.aftab2022@gmail.com>
2024-12-31 14:10:52 +0500

    update generate-ideas-page according to latest changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,9 +15,9 @@ export default function Sidebar() {
         onClick={toggleMenu}
       >
         {isMenuOpen ? (
-          <i className="fas fa-times text-darkshade text-2xl"></i>
+          <i className="fas fa-times text-2xl text-primary"></i>
         ) : (
-          <i className="fas fa-bars text-darkshade text-2xl"></i>
+          <i className="fas fa-bars text-2xl text-primary"></i>
         )}
       </button>
 
@@ -25,9 +25,9 @@ export default function Sidebar() {
       <aside
         className={`${
           isMenuOpen
-            ? 'translate-x-0 bg-darkbg'
-            : 'bg-darkshade -translate-x-full'
-        } text-darkshade md:bg-darkshade fixed left-0 top-0 z-40 h-full w-60 transform transition-transform duration-300 md:relative md:block md:translate-x-0`}
+            ? 'translate-x-0 bg-primary-light'
+            : '-translate-x-full bg-secondary-light'
+        } fixed left-0 top-0 z-40 h-full w-60 transform text-primary transition-transform duration-300 md:relative md:block md:translate-x-0 md:bg-secondary-light`}
       >
         {/* Top Section */}
         <div className="space-y-6 p-4">
@@ -39,7 +39,7 @@ export default function Sidebar() {
 
           <div className="relative">
             <button
-              className="text-darkshade flex cursor-pointer items-center space-x-3 rounded-lg border-none bg-white p-2 font-bold focus:outline-none"
+              className="flex cursor-pointer items-center space-x-3 rounded-lg border-none bg-white p-2 font-bold text-primary focus:outline-none"
               onClick={toggleDropdown}
             >
               <img
@@ -55,14 +55,14 @@ export default function Sidebar() {
               ></i>
             </button>
             {isDropdownOpen && (
-              <div className="mt-2 space-y-2 rounded-md bg-lightbg p-2 shadow-lg">
-                <div className="cursor-pointer px-4 py-2 hover:bg-darkbg">
+              <div className="mt-2 space-y-2 rounded-md bg-secondary-light p-2 shadow-lg">
+                <div className="cursor-pointer px-4 py-2 hover:bg-primary-light">
                   Option 1
                 </div>
-                <div className="cursor-pointer px-4 py-2 hover:bg-darkbg">
+                <div className="cursor-pointer px-4 py-2 hover:bg-primary-light">
                   Option 2
                 </div>
-                <div className="cursor-pointer px-4 py-2 hover:bg-darkbg">
+                <div className="cursor-pointer px-4 py-2 hover:bg-primary-light">
                   Option 3
                 </div>
               </div>
@@ -71,16 +71,16 @@ export default function Sidebar() {
 
           {/* Menu Items */}
           <nav className="space-y-4">
-            <div className="flex cursor-pointer items-center space-x-3 rounded-lg p-2 hover:bg-lightbg sm:hover:bg-darkbg">
-              <i className="fas fa-bahai text-darkshade"></i>
+            <div className="flex cursor-pointer items-center space-x-3 rounded-lg p-2 hover:bg-secondary-light sm:hover:bg-primary-light">
+              <i className="fas fa-bahai text-primary"></i>
               <span>Generate Ideas</span>
             </div>
-            <div className="flex cursor-pointer items-center space-x-3 rounded-lg p-2 hover:bg-lightbg sm:hover:bg-darkbg">
-              <i className="fas fa-bookmark text-darkshade"></i>
+            <div className="flex cursor-pointer items-center space-x-3 rounded-lg p-2 hover:bg-secondary-light sm:hover:bg-primary-light">
+              <i className="fas fa-bookmark text-primary"></i>
               <span>Saved Ideas</span>
             </div>
-            <div className="flex cursor-pointer items-center space-x-3 rounded-lg p-2 hover:bg-lightbg sm:hover:bg-darkbg">
-              <i className="fas fa-circle-up text-darkshade"></i>
+            <div className="flex cursor-pointer items-center space-x-3 rounded-lg p-2 hover:bg-secondary-light sm:hover:bg-primary-light">
+              <i className="fas fa-circle-up text-primary"></i>
               <span>Upgrade Account</span>
             </div>
           </nav>
@@ -90,12 +90,12 @@ export default function Sidebar() {
         <div className="p-4">
           <hr className="my-4 border-gray-500" />
           <nav className="space-y-4">
-            <div className="flex cursor-pointer items-center space-x-3 rounded-lg p-2 hover:bg-lightbg sm:hover:bg-darkbg">
-              <i className="far fa-circle-question text-darkshade"></i>
+            <div className="flex cursor-pointer items-center space-x-3 rounded-lg p-2 hover:bg-secondary-light sm:hover:bg-primary-light">
+              <i className="far fa-circle-question text-primary"></i>
               <span>Need Help?</span>
             </div>
-            <div className="flex cursor-pointer items-center space-x-3 rounded-lg p-2 hover:bg-lightbg sm:hover:bg-darkbg">
-              <i className="fas fa-right-to-bracket text-darkshade"></i>
+            <div className="flex cursor-pointer items-center space-x-3 rounded-lg p-2 hover:bg-secondary-light sm:hover:bg-primary-light">
+              <i className="fas fa-right-to-bracket text-primary"></i>
               <span>Sign Out</span>
             </div>
           </nav>
--- src/pages/GenerateIdea.jsx
+++ src/pages/GenerateIdea.jsx
@@ -2,23 +2,23 @@ import Sidebar from '../components/Sidebar';
 
 export default function GenerateIdea() {
   return (
-    <div className="flex min-h-screen bg-lightbg">
+    <div className="flex min-h-screen bg-secondary-light">
       <Sidebar />
       <main className="mt-10 flex-1 p-5 md:ml-10">
-        <h1 className="text-darkshade mb-6 text-left text-3xl font-bold">
+        <h1 className="mb-6 text-left text-3xl font-bold text-primary-dark">
           Generate Ideas
         </h1>
 
         {/* Main Content Box */}
         <div>
           <div className="w-full max-w-3xl rounded-lg bg-white p-10 text-center shadow-lg">
-            <div className="text-darkshade mb-6">
-              <i className="fa-solid fa-file-import mb-4 text-6xl hover:text-darkbg"></i>
-              <p className="text-darkshade text-lg font-bold">
+            <div className="mb-6 text-primary-dark">
+              <i className="fa-solid fa-file-import mb-4 text-6xl hover:text-primary-light"></i>
+              <p className="text-lg font-bold text-primary-dark">
                 Please wait while we are fetching your blog posts....
               </p>
             </div>
-            <p className="text-darkshade text-sm">
+            <p className="text-sm text-primary-dark">
               This may take a few seconds.
             </p>
           </div>
