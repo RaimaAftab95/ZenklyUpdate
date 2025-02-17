3e65fcca95cd360099da8e85fa4cea6c034f73b8
RaimaAftab95 <raima.aftab2022@gmail.com>
2025-02-17 15:54:31 +0500

    update sidebar

--- src/components/ProgressBox.jsx
+++ src/components/ProgressBox.jsx
@@ -2,12 +2,12 @@ import PropTypes from 'prop-types';
 
 export default function ProgressBox({ progress }) {
   return (
-    <div className="border-secondary-lighter mx-4 rounded-lg border p-4 shadow-md">
+    <div className="border-primary mx-4 rounded-lg border p-4 shadow-md">
       <div className="flex items-center justify-between">
         <div className="relative flex items-center justify-center">
           <svg className="h-12 w-12">
             <circle
-              className="text-lightgraytext"
+              className="text-gray-400"
               strokeWidth="4"
               stroke="currentColor"
               fill="transparent"
@@ -16,7 +16,7 @@ export default function ProgressBox({ progress }) {
               cy="24"
             />
             <circle
-              className="text-primary-dark"
+              className="text-secondary"
               strokeWidth="4"
               strokeDasharray="125.6"
               strokeDashoffset={125.6 - (125.6 * progress) / 100}
@@ -29,21 +29,23 @@ export default function ProgressBox({ progress }) {
             />
           </svg>
 
-          <span className="text-xs font-semibold text-black">Credits</span>
+          <span className="text-sm leading-tight font-medium tracking-normal text-black">
+            Credits
+          </span>
         </div>
         <div className="flex flex-col items-start">
-          <button className="border-primary-lighter text-primary-lighter mt-2 rounded-lg border px-1 py-1 text-xs font-semibold">
+          <button className="border-primary-lighter text-primary-lighter mt-2 rounded-lg border p-2 text-xs leading-none font-medium tracking-normal">
             Upgrade
           </button>
         </div>
       </div>
-      <div className="mt-4 flex justify-between text-xs text-gray-600">
+      <div className="mt-4 flex justify-between text-sm leading-tight font-medium text-gray-600">
         <span>Total</span>
-        <span className="font-semibold">50</span>
+        <span>50</span>
       </div>
-      <div className="flex justify-between text-xs text-gray-600">
+      <div className="flex justify-between text-sm leading-tight font-medium text-gray-600">
         <span>Remaining</span>
-        <span className="font-semibold">38</span>
+        <span>38</span>
       </div>
     </div>
   );
